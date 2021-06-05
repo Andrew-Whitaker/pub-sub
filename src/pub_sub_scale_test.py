95eb284cb0cfe199966aff2ce0f655ade8befd88
Jake Sutton <jrsutton@ucsd.edu>
2021-06-05 00:12:21 -0700

    update read me start consumers and publishers separately

--- src/pub_sub_scale_test.py
+++ src/pub_sub_scale_test.py
@@ -11,9 +11,23 @@ from multiprocessing import Process
 def valid_duration_str(d):
     return d.replace('.','',1).isdigit()
 
+def run_producers(procs, hosts, count, duration):
+    for i in range(0, int(count)):
+        topics = ["a{}".format(i)]
+        pubProcess = Process(target=run_publisher, args=(i, topics, hosts, duration, "logs/pubs/{}.txt".format(i)))
+        procs.append(pubProcess)
+        pubProcess.start()
+
+def run_consumers(procs, hosts, count, duration):
+    for i in range(0, int(count)):
+        topics = ["a{}".format(i)]
+        consProcess  = Process(target=run_consumer, args=(i, topics, hosts, duration, "logs/cons/{}.txt".format(i)))
+        procs.append(consProcess)
+        consProcess.start()
+
 if __name__ == "__main__":
     if len(sys.argv) != 4:
-        print("Usage: python src/pub_sub_scale_test.py <zk_config> <no. publishers and consumers> <duration - seconds>") 
+        print("Usage: python src/pub_sub_scale_test.py <zk_config> <no. pubs and cons> <duration - seconds>") 
         exit(1)
 
     # ZooKeeper Config and PubSubClient
@@ -25,18 +39,12 @@ if __name__ == "__main__":
         print("Usage: python src/pub_sub_scale_test.py <zk_config> <no. publishers and consumers>") 
         exit(1)
 
+    hosts = get_zookeeper_hosts(zk_config_path)
     procs = []
     duration = float(duration)
-    for i in range(0, int(count)):
-        topics = ["a{}".format(i)]
-        hosts  = get_zookeeper_hosts(zk_config_path)
-        pubProcess = Process(target=run_publisher, args=(i, topics, hosts, duration, "logs/pubs/{}.txt".format(i)))
-        pubProcess.start()
-        procs.append(pubProcess)
-        time.sleep(2)
-        consProcess  = Process(target=run_consumer, args=(i, topics, hosts, duration, "logs/cons/{}.txt".format(i)))
-        consProcess.start()
-        procs.append(consProcess)
+    run_producers(procs, hosts, count, duration)
+    time.sleep(2)
+    run_consumers(procs, hosts, count, duration)
 
     [p.join() for p in procs]
     for i in range(0, int(count)):
